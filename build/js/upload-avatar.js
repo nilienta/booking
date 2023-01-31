5b23a53add948086f4ac7c531eed5ea438c3495d
nilienta <97403514+nilienta@users.noreply.github.com>
2023-01-31 22:00:38 +0400

    refactoring upload avatar

--- build/js/upload-avatar.js
+++ build/js/upload-avatar.js
@@ -1,16 +1,16 @@
 const uploadAvatar = () => {
   const FILE_TYPES = ["gif", "jpg", "jpeg", "png"];
-  const buttonFileLoader = document.querySelector(".ad-form-header__input");
-  const previewAvatar = document.querySelector(".ad-form-header__preview img");
+  const buttonFileLoader = document.querySelector("#avatar");
+  const previewAvatar = document.querySelector("#avatar-preview");
 
   buttonFileLoader.addEventListener("change", () => {
     const file = buttonFileLoader.files[0];
     const fileName = file.name.toLowerCase();
 
-    const matches = FILE_TYPES.some((it) => {
-      return fileName.endsWith(it);
-    });
-    if (matches) {
+    const formatCorresponds = FILE_TYPES.some((item) =>
+      fileName.endsWith(item)
+    );
+    if (formatCorresponds) {
       const reader = new FileReader();
       reader.addEventListener("load", () => {
         previewAvatar.src = reader.result;
