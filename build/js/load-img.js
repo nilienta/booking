8fb94a509885c1c57e52a6ebeb0d8956d5985083
nilienta <97403514+nilienta@users.noreply.github.com>
2023-02-16 11:41:01 +0400

    delete unnecessary files

--- build/js/load-img.js
+++ build/js/load-img.js
@@ -1,24 +1,23 @@
 const loadImg = (inputFile, runAfterLoadFile) => {
-  const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-  const fileAvatar = inputFile.files[0];
-  const nameFileAvatar = fileAvatar?.name?.toLowerCase();
-  const isFormatFileCorrect = FILE_TYPES.some((item) =>
-    nameFileAvatar?.endsWith(item)
-  );
-
-  if (isFormatFileCorrect) {
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(fileAvatar);
-    fileReader.addEventListener('load', () => {
-      runAfterLoadFile(fileReader);
-    });
-    fileReader.addEventListener('error', () => {
-      console.error('Произошла ошибка при чтении файла');
-    });
-  } else {
-    inputFile.setCustomValidity('Расширение файла не корректно');
-    inputFile.reportValidity();
-  }
+  //   const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  //   const fileAvatar = inputFile.files[0];
+  //   const nameFileAvatar = fileAvatar?.name?.toLowerCase();
+  //   const isFormatFileCorrect = FILE_TYPES.some((item) =>
+  //     nameFileAvatar?.endsWith(item)
+  //   );
+  //   if (isFormatFileCorrect) {
+  //     const fileReader = new FileReader();
+  //     fileReader.readAsDataURL(fileAvatar);
+  //     fileReader.addEventListener('load', () => {
+  //       runAfterLoadFile(fileReader);
+  //     });
+  //     fileReader.addEventListener('error', () => {
+  //       console.error('Произошла ошибка при чтении файла');
+  //     });
+  //   } else {
+  //     inputFile.setCustomValidity('Расширение файла не корректно');
+  //     inputFile.reportValidity();
+  //   }
 };
 
 export default loadImg;
