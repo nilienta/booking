8e0c0db1bca3ebd3bf5418f47b6de72354f6486e
nilienta <97403514+nilienta@users.noreply.github.com>
2023-02-21 10:31:10 +0400

    Improvement createWindowError: added functions to close the window by ESC and by click

--- build/js/create-window-error.js
+++ build/js/create-window-error.js
@@ -1,11 +1,36 @@
+const messageDeleteHandler = (messageElement) => {
+  messageElement.remove();
+};
+const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
+const onPopupEscKeydown = (evt) => {
+  const body = document.querySelector('body');
+  if (isEscEvent(evt)) {
+    messageDeleteHandler();
+    body.removeEventListener('keydown', onPopupEscKeydown);
+  }
+};
+
 const createWindowError = () => {
   const divErrorFromBody = document
     .querySelector('#error')
     .content.querySelector('.error')
     .cloneNode(true);
 
-  const mainFromBody = document.querySelector('main');
-  mainFromBody.prepend(divErrorFromBody);
+  const body = document.querySelector('body');
+  body.prepend(divErrorFromBody);
+
+  body.addEventListener('click', messageDeleteHandler(divErrorFromBody), {
+    once: true,
+  });
+  body.addEventListener('keydown', onPopupEscKeydown());
+  const ButtonCloseError = divErrorFromBody.querySelector('.error__button');
+  ButtonCloseError.addEventListener(
+    'click',
+    messageDeleteHandler(divErrorFromBody),
+    {
+      once: true,
+    }
+  );
 };
 
 export default createWindowError;
