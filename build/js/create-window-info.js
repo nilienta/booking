df4ffadd075178665a556708ecae4624c048e31b
nilienta <97403514+nilienta@users.noreply.github.com>
2023-02-21 15:51:46 +0400

    Fixing the modal window closing trigger

--- build/js/create-window-info.js
+++ build/js/create-window-info.js
@@ -1,48 +1,45 @@
-// TODO вынести общие функции в отдельный файл
 const body = document.querySelector('body');
 
-const messageDeleteHandler = (messageElement) => {
-  messageElement.remove();
+const deleteModalMessage = () => {
+  const divSuccess = document.querySelector('.success');
+  const divError = document.querySelector('.error');
+  divSuccess?.remove();
+  divError?.remove();
+  body.removeEventListener('click', deleteModalMessage);
+  // FIXME убрать взаимосвзяь функций
+  body.removeEventListener('keydown', onPopupEscKeydown);
 };
+
 const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 const onPopupEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
-    messageDeleteHandler();
-    body.removeEventListener('keydown', onPopupEscKeydown);
+    deleteModalMessage();
   }
 };
-
-const createWindowError = () => {
+// FIXME дублирование кода createWindowError и createWindowSuccess
+const createWindowError = (textError) => {
   const divErrorFromBody = document
     .querySelector('#error')
     .content.querySelector('.error')
     .cloneNode(true);
-
+  const pTextError = divErrorFromBody.querySelector('.error__message');
+  pTextError.textContent = `${pTextError.textContent}. Текст ошибки: ${textError}`;
   body.appendChild(divErrorFromBody);
 
-  body.addEventListener('click', messageDeleteHandler(divErrorFromBody), {
-    once: true,
-  });
-  body.addEventListener('keydown', onPopupEscKeydown());
+  body.addEventListener('click', deleteModalMessage);
+  body.addEventListener('keydown', onPopupEscKeydown);
   const ButtonCloseError = divErrorFromBody.querySelector('.error__button');
-  ButtonCloseError.addEventListener(
-    'click',
-    messageDeleteHandler(divErrorFromBody),
-    {
-      once: true,
-    }
-  );
+  ButtonCloseError.addEventListener('click', deleteModalMessage);
 };
+
 const createWindowSuccess = () => {
   const divSuccessFromBody = document
     .querySelector('#success')
     .content.querySelector('.success')
     .cloneNode(true);
-
   body.appendChild(divSuccessFromBody);
-  body.addEventListener('click', messageDeleteHandler(divSuccessFromBody), {
-    once: true,
-  });
+
+  body.addEventListener('click', deleteModalMessage);
   body.addEventListener('keydown', onPopupEscKeydown);
 };
 
