d43e1b1f837b69c3e36187773bba4c8d6d90b0c6
nilienta <97403514+nilienta@users.noreply.github.com>
2023-02-25 23:37:35 +0400

    Change the data processing from then to async in main module

--- src/js/main.js
+++ src/js/main.js
@@ -3,32 +3,27 @@ import markersList from './edit-marker-list.js';
 import sortingAd from './sorting.js';
 import runValidateForm from './validate-form.js';
 import { fetchRequest } from './api.js';
-import showToast from './toast.js';
 import { onFormSubmit } from './submit-form.js';
 
 const inputFileAvatarFromBody = document.querySelector('#avatar');
 inputFileAvatarFromBody.addEventListener('change', (evt) => {
   loadAvatar(evt);
 });
+
 onFormSubmit();
 runValidateForm();
 
 const BOOKING_URL_DATA =
   'https://23.javascript.pages.academy/keksobooking/data';
 
-let adList = [];
-fetchRequest(BOOKING_URL_DATA)
-  .then((res) => {
-    if (res) {
-      adList = res;
-      markersList.add(res);
-    }
-  })
-  .catch((error) => {
-    showToast('Произошла ошибка!', error.message);
-  });
+const getAdList = async () => {
+  const adList = await fetchRequest(BOOKING_URL_DATA);
+  markersList.add(adList);
 
-const formFiltersFromBody = document.querySelector('.map__filters');
-formFiltersFromBody.addEventListener('change', () => {
-  sortingAd(adList);
-});
+  const formFiltersFromBody = document.querySelector('.map__filters');
+  formFiltersFromBody.addEventListener('change', () => {
+    sortingAd(adList);
+  });
+};
+
+getAdList();
