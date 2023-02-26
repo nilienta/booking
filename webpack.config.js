3f85014775e9f3db3a9d29e93ab9a3d340945aa4
nilienta <97403514+nilienta@users.noreply.github.com>
2023-02-26 18:00:53 +0400

    Fix webpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,35 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'src'),
     },
     compress: true,
     port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+      },
+    ],
   },
 };
