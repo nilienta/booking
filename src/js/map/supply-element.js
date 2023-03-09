b1132e90a6cdd3ddcb7e8f16ae797cee440fab96
nilienta <97403514+nilienta@users.noreply.github.com>
2023-03-09 23:18:21 +0400

    nesting reduction createPopupAd

--- src/js/map/supply-element.js
+++ src/js/map/supply-element.js
@@ -5,34 +5,33 @@ import {
 } from './fill-popup.js';
 
 const createPopupAd = (elem) => {
+  const offer = elem?.offer;
+  if (!offer) return;
+
   const articlePopupFromTemplateCard = document
     .querySelector('#card')
     .content.querySelector('.popup');
   const articlePopupAd = articlePopupFromTemplateCard.cloneNode(true);
 
-  const offer = elem?.offer;
-  if (offer) {
-    const fillTextContent = (querySelector, metricAd) => {
-      articlePopupAd.querySelector(querySelector).textContent = metricAd;
-    };
-    fillTextContent('.popup__title', offer.title);
-    fillTextContent('.popup__text--address', offer.address);
-    fillTextContent('.popup__text--price', `${offer.price} р/ночь`);
-    fillTextContent('.popup__type', offer.type);
-    fillTextContent(
-      '.popup__text--capacity',
-      `${offer.rooms} комн. для ${offer.guests} чел.`
-    );
-    fillTextContent(
-      '.popup__text--time',
-      `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`
-    );
-    fillTextContent('.popup__description', offer.description);
+  const fillTextContent = (querySelector, metricAd) => {
+    articlePopupAd.querySelector(querySelector).textContent = metricAd;
+  };
+  fillTextContent('.popup__title', offer.title);
+  fillTextContent('.popup__text--address', offer.address);
+  fillTextContent('.popup__text--price', `${offer.price} р/ночь`);
+  fillTextContent('.popup__type', offer.type);
+  fillTextContent(
+    '.popup__text--capacity',
+    `${offer.rooms} комн. для ${offer.guests} чел.`
+  );
+  fillTextContent(
+    '.popup__text--time',
+    `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`
+  );
+  fillTextContent('.popup__description', offer.description);
 
-    addPhotosInPopup(articlePopupAd, offer);
-    addFeaturesInPopup(articlePopupAd, offer);
-  }
-
+  addPhotosInPopup(articlePopupAd, offer);
+  addFeaturesInPopup(articlePopupAd, offer);
   addAvatarInPopup(articlePopupAd, elem);
 
   return articlePopupAd;
